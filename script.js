97d42f04b9a85519bcf197d7ebde2c962011f74c
ngarikev <kevin.gary191@gmail.com>
2024-09-11 16:07:06 +0300

    updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const closeNavEl = document.getElementById("close-nav");
 const openNavEl = document.getElementById("open-nav");
 const navBarEl = document.querySelector('.nav-links ul');
-const currentYearEl = document.getElementById("current-year")
+// const currentYearEl = document.getElementById("current-year")
 
 
 
@@ -15,12 +15,12 @@ closeNavEl.addEventListener("click", hideMenu)
  function hideMenu(){
     navBarEl.style.right = "-250px"
  }
- 
- function showCurrentYear() {
-    currentYearEl.textContent = new Date().getFullYear()
-}
 
-showCurrentYear()
+//  function showCurrentYear() {
+//     currentYearEl.textContent = new Date().getFullYear()
+// }
+
+// showCurrentYear()
 
 //for a fixed navbar on scroll
 
